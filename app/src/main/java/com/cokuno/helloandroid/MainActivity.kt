f7dad58a4de579c3032732bfa33eb14534c3df8b
Parpadelle <cokuno@u.rochester.edu>
2023-01-23 16:26:59 -0500

    Final commit, all code is finished to my knowledge though the emulator does not work.

--- app/src/main/java/com/cokuno/helloandroid/MainActivity.kt
+++ app/src/main/java/com/cokuno/helloandroid/MainActivity.kt
@@ -3,14 +3,24 @@ package com.cokuno.helloandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameTextView: TextView
+    private lateinit var answerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameTextView = findViewById(R.id.hello_android_one)
+        answerButton = findViewById(R.id.button1)
     }
 
-    fun onAnswerButton(view: View) {}
-
+    fun onAnswerButton(view: View) {
+        nameTextView.text = getString(R.string.hello_android_two)
+    }
 
 }
